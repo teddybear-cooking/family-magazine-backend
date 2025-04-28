572e541331bd8909e556a36a2a5339dd2bead5e3
teddybear-cooking <socalledjamesakp@gmail.com>
2025-04-28 20:40:47 +0700

    first commit

---
+++ src/main/java/com/arkar/family_magazine/FamilyMagazineApplication.java
@@ -0,0 +1,13 @@
+package com.arkar.family_magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyMagazineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arkar/family_magazine/FamilyMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkar.family_magazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
